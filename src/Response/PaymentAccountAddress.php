f280ef8a90c1420f2aa6074cadb2faa4fea4acfb
Jason Judge <jason.judge@consil.co.uk>
2018-01-10 19:19:06 +0000

    Add support for PaymentAccountAddress

---
+++ src/Response/PaymentAccountAddress.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Consilience\Starling\Payments\Response;
+
+/**
+ * Unique address of a payment account, this is UK account number
+ * and sort code from which payments can be sent and received.
+ */
+
+use Consilience\Starling\Payments\HydratableTrait;
+use Carbon\Carbon;
+
+class PaymentAccountAddress
+{
+    use HydratableTrait;
+
+    /**
+     * @var string
+     */
+    const STATUS_ACTIVE         = 'ACTIVE';
+    const STATUS_INSTRUCT_ONLY  = 'INSTRUCT_ONLY';
+    const STATUS_CLOSED         = 'CLOSED';
+    const STATUS_DECEASED       = 'DECEASED';
+
+    /**
+     * @var string UUID
+     * Unique identifier of the company requesting or receiving the payment.
+     */
+    protected $paymentBusinessUid;
+
+    /**
+     * @var string UUID
+     * Unique identifier of the account containing the funds sent or received.
+     */
+    protected $accountUid;
+
+    /**
+     * @var string UUID
+     * Unique identifier of the payment account address.
+     */
+    protected $addressUid;
+
+    /**
+     * @var string e.g. 040030
+     * Sort code for the account, used to receive payments into the account.
+     */
+    protected $sortCode;
+
+    /**
+     * @var string e.g. 11223344
+     * Account number, used to receive payments into the account.
+     */
+    protected $accountNumber;
+
+    /**
+     * @var string date-time e.g. 2017-06-05T11:47:58.801Z
+     * Date and time the account was first created.
+     */
+    protected $createdAt;
+
+    /**
+     * @var string
+     * Name for the account, this will be included in outbound faster
+     * payment messages to other recipients.
+     */
+    protected $accountName;
+
+    /**
+     * @var string one of static::STATUS_*
+     * Status of the payment request.
+     */
+    protected $status;
+
+    /**
+     * @return Carbon the createdAt as a Carbon object, with timezone preserved.
+     */
+    public function getCreatedAtCarbon()
+    {
+        return Carbon::parse($this->createdAt);
+    }
+}
---
+++ tests/Response/PaymentAccountAddressTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Consilience\Starling\Payments\Response;
+
+/**
+ *
+ */
+
+use PHPUnit\Framework\TestCase;
+use Money\Formatter\DecimalMoneyFormatter;
+use Money\Currencies\ISOCurrencies;
+use Money\Money;
+use Carbon\Carbon;
+
+class PaymentAccountAddressTest extends TestCase
+{
+    /**
+     * Can instantiate an empty object.
+     */
+    public function testEmpty()
+    {
+        $paymentAccountAddress = new PaymentAccountAddress();
+
+        $this->assertTrue($paymentAccountAddress instanceof PaymentAccountAddress);
+    }
+
+    /**
+     * Read and return the contents of a test JSON file.
+     */
+    protected function readDataFile($filename)
+    {
+        $dataJson = file_get_contents(__DIR__ . '/../data/' . $filename);
+        return json_decode($dataJson, true);
+    }
+
+    /**
+     * Can instantiate from a single data record.
+     */
+    public function testFromArray()
+    {
+        $paymentAccountAddressData = $this->readDataFile('paymentAccountAddress.json');
+
+        $paymentAccountAddress = PaymentAccountAddress::fromArray($paymentAccountAddressData);
+
+        $this->assertTrue($paymentAccountAddress instanceof PaymentAccountAddress);
+        $this->assertSame('2017-06-05T11:47:58.801Z', $paymentAccountAddress->createdAt);
+        $this->assertTrue($paymentAccountAddress->createdAtCarbon instanceof Carbon);
+        $this->assertSame(
+            '2017-06-05T11:47:58+00:00',
+            $paymentAccountAddress->createdAtCarbon->toRfc3339String()
+        );
+    }
+}
