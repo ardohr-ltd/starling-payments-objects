5cde330f09253a751fabc0418b759f132a3d50a2
Jason Judge <jason.judge@consil.co.uk>
2018-01-24 17:52:47 +0000

    Added CreatePaymentAccountAddress (not tested)

---
+++ src/Request/CreatePaymentAccountAddress.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Consilience\Starling\Payments\Request;
+
+/**
+ * Request to get a single payment account.
+ */
+
+use Consilience\Starling\Payments\Request\Model\Endpoint;
+use Consilience\Starling\Payments\AbstractRequest;
+use UnexpectedValueException;
+
+use Consilience\Starling\Payments\Request\Model\CreatePaymentAccountAddressRequest;
+
+class CreatePaymentAccountAddress extends AbstractRequest
+{
+    /**
+     * @inherit
+     */
+    protected $pathTemplate = 'account/{accountUid}/address/{addressUid}';
+
+    protected $httpMethod = 'PUT';
+
+    protected $accountUid;
+    protected $addressUid;
+    protected $createPaymentAccountAddressRequest;
+
+    /**
+     * @param string $paymentBusinessUid
+     * @param string $accountUid the accound to create the address for
+     * @param string $addressUid the new UID to assign to the address
+     */
+    public function __construct(
+        Endpoint $endpoint,
+        $accountUid,
+        $addressUid,
+        CreatePaymentAccountAddressRequest $createPaymentAccountAddressRequest
+    ) {
+        $this->setEndpoint($endpoint);
+        $this->setAccountUid($accountUid);
+        $this->setAddressUid($addressUid);
+        $this->setCreatePaymentAccountAddressRequest($createPaymentAccountAddressRequest);
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAccountUid($value)
+    {
+        $this->accountUid = $value;
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAddressUid($value)
+    {
+        $this->addressUid = $value;
+    }
+
+    /**
+     * @param CreatePaymentAccountAddressRequest
+     */
+    protected function setAddressUid(CreatePaymentAccountAddressRequest $value)
+    {
+        $this->createPaymentAccountAddressRequest = $value;
+    }
+}
