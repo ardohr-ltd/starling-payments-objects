6fd5589f3c02730110606f8f75acfd56a1518636
Jason Judge <jason.judge@consil.co.uk>
2018-01-24 18:15:35 +0000

    Added GetPayments request

---
+++ src/Request/GetPayments.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Consilience\Starling\Payments\Request;
+
+/**
+ * Request to get a single payment account.
+ */
+
+use Consilience\Starling\Payments\Request\Model\Endpoint;
+use Consilience\Starling\Payments\AbstractRequest;
+use UnexpectedValueException;
+
+class GetPayments extends AbstractRequest
+{
+    /**
+     * @inherit
+     */
+    protected $pathTemplate = 'account/{accountUid}/address/{addressUid}/payment';
+
+    protected $httpMethod = 'GET';
+
+    protected $accountUid;
+    protected $addressUid;
+
+    /**
+     * @param string $paymentBusinessUid
+     * @param string $accountUid the accound to retrieve
+     */
+    public function __construct(Endpoint $endpoint, $accountUid, $addressUid)
+    {
+        $this->setEndpoint($endpoint);
+        $this->setAccountUid($accountUid);
+        $this->setAddressUid($addressUid);
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAccountUid($value)
+    {
+        $this->accountUid = $value;
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAddressUid($value)
+    {
+        $this->addressUid = $value;
+    }
+}
