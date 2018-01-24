6a1704e8644502e66e4975046c3a82db44713237
Jason Judge <jason.judge@consil.co.uk>
2018-01-24 18:36:16 +0000

    Added GetPayment (no tests yet)

---
+++ src/Request/GetPayment.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Consilience\Starling\Payments\Request;
+
+/**
+ * Request to get a single payment account.
+ */
+
+use Consilience\Starling\Payments\Request\Model\Endpoint;
+use Consilience\Starling\Payments\AbstractRequest;
+use UnexpectedValueException;
+
+class GetPayment extends AbstractRequest
+{
+    /**
+     * @inherit
+     */
+    protected $pathTemplate = 'account/{accountUid}/address/{addressUid}/payment/{paymentUid}';
+
+    protected $httpMethod = 'GET';
+
+    protected $accountUid;
+    protected $addressUid;
+    protected $paymentUid;
+
+    /**
+     * @param string $paymentBusinessUid
+     * @param string $accountUid the accound to retrieve
+     */
+    public function __construct(
+        Endpoint $endpoint,
+        $accountUid,
+        $addressUid,
+        $paymentUid
+    ) {
+        $this->setEndpoint($endpoint);
+        $this->setAccountUid($accountUid);
+        $this->setAddressUid($addressUid);
+        $this->setPaymentUid($paymentUid);
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAccountUid($value)
+    {
+        $this->accountUid = $value;
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setAddressUid($value)
+    {
+        $this->addressUid = $value;
+    }
+
+    /**
+     * @param string UUID
+     */
+    protected function setPaymentUid($value)
+    {
+        $this->paymentUid = $value;
+    }
+}
